e55da7d5d2cebf772ef127033c7680b728b4574a
@FranCarn <carnevalefranco99@gmail.com>
2023-06-13 14:33:24 -0300

    product card ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layouts";
-import {
-  Card,
-  CardActionArea,
-  CardMedia,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Typography } from "@mui/material";
 import { initialData } from "../database/products";
+import { ProductList } from "../components/products";
 
 const Home: NextPage = () => {
   return (
@@ -22,21 +17,7 @@ const Home: NextPage = () => {
         <Typography variant="h2" sx={{ mb: 1 }}>
           All products
         </Typography>
-        <Grid container spacing={4}>
-          {initialData.products.map((product) => (
-            <Grid item key={product.slug} xs={6} sm={4}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={`products/${product.images[0]}`}
-                    alt={product.title}
-                  />
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+        <ProductList products={initialData.products as any} />
       </>
     </ShopLayout>
   );
