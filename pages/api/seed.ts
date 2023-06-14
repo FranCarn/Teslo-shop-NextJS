fcf2df3127037a1fe2f56d7625bf71ffb4a8737d
@FranCarn <carnevalefranco99@gmail.com>
2023-06-14 11:28:03 -0300

    db connect ready, first endpoint to fill the database ready

---
+++ pages/api/seed.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db, seedDatabase } from "../../database";
+import { Product } from "../../models";
+
+type Data = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (process.env.NODE_ENV === "production") {
+    return res
+      .status(401)
+      .json({ message: "User has no acces to this service" });
+  }
+
+  switch (req.method) {
+    case "GET":
+      await db.connect();
+      // Delete all Entries
+      await Product.deleteMany();
+      // Insert data to fill DB
+      await Product.insertMany(seedDatabase.initialData.products);
+
+      await db.disconnect();
+      return res.status(200).json({ message: "Process succesfully" });
+
+    default:
+      return res.status(400).json({ message: "Method is not allowed" });
+  }
+}
