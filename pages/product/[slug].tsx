8885ba602a5185d92bc3bb5fa9d9a3321ccc70b6
@FranCarn <carnevalefranco99@gmail.com>
2023-06-15 10:53:55 -0300

    get product using ssr ready

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { GetServerSideProps, NextPage } from "next";
 import { ShopLayout } from "../../components/layouts";
-import { initialData } from "../../database/products";
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import { ProductSlideshow, SizeSelector } from "../../components/products";
 import { ItemCounter } from "../../components/ui";
+import { IProduct } from "../../interfaces";
+import { dbProducts } from "../../database";
 
-const product = initialData.products[0];
+interface Props {
+  product: IProduct;
+}
 
-const ProductPage = () => {
+const ProductPage: NextPage<Props> = ({ product }) => {
   return (
     <ShopLayout title={product.title} pageDescription={product.description}>
       <Grid container spacing={3}>
@@ -59,6 +63,24 @@ const ProductPage = () => {
       </Grid>
     </ShopLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { slug = "" } = params as { slug: string };
+  const product = await dbProducts.getProductBySlug(slug);
+  if (!product) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      product,
+    },
+  };
 };
 
 export default ProductPage;
