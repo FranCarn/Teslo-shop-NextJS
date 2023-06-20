0c5411a0c85c15645062cd80fbf61103c22c332d
@FranCarn <carnevalefranco99@gmail.com>
2023-06-20 13:39:48 -0300

    test users done

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { db, seedDatabase } from "../../database";
-import { Product } from "../../models";
+import { Product, User } from "../../models";
 
 type Data = {
   message: string;
@@ -19,9 +19,9 @@ export default async function handler(
   switch (req.method) {
     case "GET":
       await db.connect();
-      // Delete all Entries
+      await User.deleteMany();
+      await User.insertMany(seedDatabase.initialData.users);
       await Product.deleteMany();
-      // Insert data to fill DB
       await Product.insertMany(seedDatabase.initialData.products);
 
       await db.disconnect();
