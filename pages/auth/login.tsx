28cbc63bef1941733507724fade1908767f34861
@FranCarn <carnevalefranco99@gmail.com>
2023-06-25 15:17:42 -0300

    validations in login form ready

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,45 +2,78 @@ import React from "react";
 import { AuthLayout } from "../../components/layouts";
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import NextLink from "next/link";
+import { useForm } from "react-hook-form";
+import { validation } from "../../utils";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onLoginUser = (data: FormData) => {};
   return (
     <AuthLayout title="Login">
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Sign in
-            </Typography>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Sign in
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                label="Email"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "Required field",
+                  validate: validation.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                variant="filled"
+                type="password"
+                fullWidth
+                {...register("password", {
+                  required: "Required field",
+                  minLength: { value: 6, message: "Password is too short" },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+                type="submit"
+              >
+                Sign in
+              </Button>
+            </Grid>
+            <Grid item xs={12} display="flex" justifyContent="center">
+              <NextLink href="/auth/register" passHref legacyBehavior>
+                <Link underline="always">Create account</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField label="Email" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Password"
-              variant="filled"
-              type="password"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Sign in
-            </Button>
-          </Grid>
-          <Grid item xs={12} display="flex" justifyContent="center">
-            <NextLink href="/auth/register" passHref legacyBehavior>
-              <Link underline="always">Create account</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
