5adb1e842cb7f1bf7218c66841b90bef2309ff08
@FranCarn <carnevalefranco99@gmail.com>
2023-07-01 14:54:35 -0300

    setting next auth credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,54 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
+    Credentials({
+      name: "Custom Login",
+      credentials: {
+        email: {
+          label: "Email:",
+          type: "email",
+          placeholder: "email@teslo.com",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "**********",
+        },
+      },
+      async authorize(credentials) {
+        return null;
+        // return {email: credentials?.email, role: 'client', };
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        switch (account.type) {
+          case "oauth":
+            break;
+
+          case "credentials":
+            token.user = user;
+            break;
+        }
+      }
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      session.user = token.user as any;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
