c1bdbe74ecc9940f9081568ec155773d82070e24
@FranCarn <carnevalefranco99@gmail.com>
2023-07-30 13:28:24 -0300

    update product rest api ready & downgrade mongoose version

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,12 +2,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { IProduct } from "../../../interfaces/products";
 import Product from "../../../models/product";
 import { db } from "../../../database/";
+import { isValidObjectId } from "mongoose";
 
 type Data =
   | {
       message: string;
     }
-  | IProduct[];
+  | IProduct[]
+  | IProduct;
 
 export default function handler(
   req: NextApiRequest,
@@ -17,7 +19,7 @@ export default function handler(
     case "GET":
       return getProducts(req, res);
     case "PUT":
-      return;
+      return updateProduct(req, res);
 
     case "POST":
       return;
@@ -36,4 +38,41 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   // TODO: update images;
 
   return res.status(200).json(products);
+};
+const updateProduct = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { _id = "", images = [] } = req.body as IProduct;
+
+  if (!isValidObjectId(_id)) {
+    return res.status(400).json({ message: "Invalid product ID" });
+  }
+
+  if (images.length <= 2) {
+    return res.status(400).json({ message: "Is necessary two images" });
+  }
+
+  // TODO: eval case localhost:3000/products/img.jpg to avoid
+  try {
+    await db.connect();
+
+    const product = await Product.findById(_id);
+
+    if (!product) {
+      await db.disconnect();
+      return res.status(400).json({ message: "Product doesnt exist" });
+    }
+
+    // TODO: delete images in Cloudinary
+
+    await product.update(req.body);
+
+    await db.disconnect();
+    return res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    await db.disconnect();
+    return res.status(400).json({ message: "Error message in server console" });
+  }
 };
